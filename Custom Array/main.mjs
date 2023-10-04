d1fa4e99c6e0abb007c4eb7270c44b80c1357e34
Prachi Mishra <143651644+misprachi023@users.noreply.github.com>
2023-10-05 00:00:56 +0530

    Update main.mjs

--- Custom Array/main.mjs
+++ Custom Array/main.mjs
@@ -1,7 +1,7 @@
 // main.mjs
 import CustomArray from './customArray.mjs';
 
-// Example usage
+
 const stack = new CustomArray(1, 2, 3, 4);
 stack.print();  // Output: [1,2,3,4]
 stack.push(5);
@@ -10,4 +10,4 @@ stack.pop();
 stack.print();  // Output: [1,2,3,4]
 stack.printReverse();  // Output: [4,3,2,1]
 console.log('Size:', stack.getSize());  // Output: Size: 4
-console.log('Top element:', stack.top());  // Output: Top element: 3
+console.log('Top element:', stack.top());  // Output: Top element: 4
